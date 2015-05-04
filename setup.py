75774567461d56355d63dc656fc8989fc2e9eba6
zar1 <zar1@wireless-s1-so-150-61-221.uchicago.edu>
2015-05-04 09:32:23 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='upsg',
+        version='0.0.1',
+        url='https://github.com/dssg/UPSG',
+        author='Data Science For Social Good',
+        description=('A set of tools and conventions to help data scientists '
+                     'share code'),
+        packages=find_packages(),
+        install_requires=('numpy',
+                          'scikit-learn', 
+                          'matplotlib', 
+                          'graphviz',
+                          'SQLAlchemy',
+                          'tables'),
+        include_package_data = True)
+
