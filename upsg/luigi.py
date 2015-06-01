30b98f94d8a8bde39debef6d1528f41a50d6149f
zar1 <zachary.rubenstein@gmail.com>
2015-06-01 16:20:23 -0500

    draft of node_to_task

--- upsg/luigi.py
+++ upsg/luigi.py
@@ -1,3 +1,5 @@
+from collections import named
+
 import luigi
 
 # We have to traverse the graph, starting with nodes that require no inputs,
@@ -14,21 +16,55 @@ import luigi
 # need some meta-information to figure out which input/output keys a given
 # file goes to. UObject should probably open a file in memory and then actually
 # write it at the last minute
+
 def node_to_task(node, context):
 
+    # we need to keep track of which node gives which output
+    node_inputs = node.get_inputs()
+    node_outputs = node.get_outputs()
     req_tasks = {key: context[conn.other.node.uid] for
-                 key, conn in get_inputs().iteritems()}
+                 key, conn in node_inputs.iteritems()}
     def requires(self):
         return req_tasks
 
     # TODO nonlocal targets
-    out_files = [luigi.LocalTarget('{}.upsg'.format(
-                    conn.edge.uid)) for conn  in get_outputs.itervalues()]
+    out_files = {key: luigi.LocalTarget('{}.upsg'.format(
+                    conn.edge.uid)) for key, conn  in node_outputs.itervalues()}
     def output(self):
         return out_files
 
+    input_keys = node_inputs.keys()
+    others_output_keys = {in_key: out_key for in_key, out_key in zip(
+        input_keys,
+        [node_inputs[key].other.node.key for key in input_keys])}
     def run(self):
+        import pdb; pdb.set_trace()
+        input_files = {in_key:
+                       self.input()[in_key][others_output_keys[in_key]].open('r') 
+                       for in_key in others_output_keys}
+        input_args = {in_key:
+                      UObject(
+                          UObjectPhase.Read, 
+                          input_files[in_key].read())}
+        [input_file_[in_key].close() for in_key in input_files]
+        output_args = node.get_stage().run(node_outputs.keys(), **input_args)
+        output_files = {out_key: self.output()[out_key].open('w') for 
+                        out_key in node_outputs}
+        [output_files[out_key].write(output_args[out_key].get_image()) for 
+         out_key in output_files]
+        [output_files[out_key].close() for out_key in output_files]
 
+    return type(
+            'Task_{}'.format(node.uid), 
+            (luigi.Task,), 
+            {'requires': requires, 'output': output, 'run': run})
 
+def to_luigi(nodes):
+    node_queue = [node for node in self.__nodes
+                  if not node.get_inputs()]  # start with the leaves
+    context = {}
+    while node_queue:
+        node = node_queue.pop()
+        task = node_to_task(node)
 
 
